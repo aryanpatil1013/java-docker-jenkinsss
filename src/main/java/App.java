a9ded6b30bf2a44584dbb41ad6774cbaa9c80bcd
aryanpatil <160165435+aryanpatil1013@users.noreply.github.com>
2026-06-21 23:24:58 +0530

    Create App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Docker and Jenkins!");
+        System.out.println("Project by Aryan Patil");
+        System.out.println("DevOps Engineer");
+    }
+}
